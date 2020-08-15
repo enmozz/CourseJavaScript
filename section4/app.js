f344765f4ae729e6051f50d4d9954b461b2f87e2
enmozz <43096220+enmozz@users.noreply.github.com>
2020-08-15 22:22:17 +0700

    4

--- section4/app.js
+++ section4/app.js
@@ -1,4 +1,37 @@
-function sayhi() {
-    alert("hi");
+function toCelsius(fahrenheit) {
+
+    let value = (fahrenheit - 32) * 5 / 9
+    return value.toFixed(2) + "°C" ;
 }
-sayhi()
+function display(id, value) {
+    document.getElementById(id).innerHTML = "<b>" + value + "</b>"
+}
+
+
+display("toCelsius", toCelsius(120) )
+
+function toFahrenheit(fahrenheit) {
+
+    let value = (fahrenheit * 9) /5 +32
+    return value.toFixed(2) + "°F" ;
+}
+
+display("toFahrenheit", toFahrenheit(48) )
+
+
+
+
+
+// function toCelsius() {
+//     let fahrenheit = prompt("Fahrenheit: ")
+//     let value = (fahrenheit - 32) * 5 / 9
+//     document.getElementById("toCelsius").innerHTML = value.toFixed(2) + "°C" ;
+// }
+// toCelsius()
+
+// function toFahrenheit() {
+//     let fahrenheit = prompt("Celsius: ")
+//     let value = (fahrenheit * 9) /5 +32
+//     document.getElementById("toFahrenheit").innerHTML = value.toFixed(2) + "°F" ;
+// }
+// toFahrenheit()
---
+++ section4/return.js
@@ -0,0 +1,5 @@
+function sayhhi(name, lastname) {
+    return "hello " + name +" " + lastname
+}
+let name = prompt("enter name :")
+alert(sayhhi(name, prompt("enter lastname :")))
